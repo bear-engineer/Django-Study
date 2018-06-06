8a8c567366f334229a0e3d888561fb19a1d76fdb
Sehyeon Youn <d.sehyeon@gmail.com>
2018-06-06 23:10:08 +0900

    render_to_string 대신에 render 을 사용하요 코드 축약

--- app/blog/views.py
+++ app/blog/views.py
@@ -7,4 +7,5 @@ from django.template.loader import render_to_string
 
 def post_list(request):
     html = render_to_string('blog/post_list.html')
-    return HttpResponse(html)
+    return render(request, 'blog/post_list.html')
+    # return HttpResponse(html)
