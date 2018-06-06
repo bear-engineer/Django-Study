bf5d8e2975b3f348e87399fa901d2830145ac247
Sehyeon Youn <d.sehyeon@gmail.com>
2018-06-06 23:47:08 +0900

    post_list 동적으로 rendering 할수있도록 작성

--- app/blog/views.py
+++ app/blog/views.py
@@ -8,8 +8,13 @@ from .models import Post
 
 
 def post_list(request):
-    post = Post.objects.all()
-
-
-    return HttpResponse(print(post))
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+    # render 는 주어진 1, 2 번째 인수를 사용해서
+    # 1번째 인수: HttpResponse인스턴스
+    # 2번째 인수: 문자열 (TEMPLATE['DIRS']를 기준으로 탐색할 템플릿 파일의 경로)
+    # 3번째 인수: 템플릿을 렌더링 할 때 사용할 객체 모음
+    return render(request,'blog/post_list.html', context)
 
